8f3ce075de7b7ec7315325af5df066e5a04930d9
manoj-bhaskaran <manoj.bhaskaran@gmail.com>
2025-01-10 22:21:27 +0530

    Removed debug statement to print type of $file

--- FileDistributor.ps1
+++ FileDistributor.ps1
@@ -369,7 +369,6 @@ function DistributeFilesToSubfolders {
                     LogMessage -Message "Copied and immediately deleted: $file to $destinationFile"
                 } elseif ($DeleteMode -eq "EndOfScript") {
                     # Add file to the list for end-of-script deletion
-                    Write-Host "DEBUG: file type: $($file.GetType())"
                     Write-Host "DEBUG: file: $file"
                     $FilesToDelete.Value += $file
                     LogMessage -Message "Copied successfully: $file to $destinationFile (pending end-of-script deletion)"
