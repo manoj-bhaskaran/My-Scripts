823d896bd084ad4c7b5db5532c757019f5492fe9
manoj-bhaskaran <manoj.bhaskaran@gmail.com>
2025-05-22 13:49:26 +0530

    Refactor: Add timeline_writer SQL script for user creation and privilege grants

---
+++ timeline_data/users/timeline_writer.sql
@@ -0,0 +1,28 @@
+-- Create timeline_writer user if it doesn't already exist
+DO
+$$
+BEGIN
+   IF NOT EXISTS (
+       SELECT FROM pg_catalog.pg_roles WHERE rolname = 'timeline_writer'
+   ) THEN
+       CREATE USER timeline_writer;
+   END IF;
+END
+$$;
+
+-- Grant minimal required privileges
+-- Create timeline_writer user if it doesn't already exist
+DO
+$$
+BEGIN
+   IF NOT EXISTS (
+       SELECT FROM pg_catalog.pg_roles WHERE rolname = 'timeline_writer'
+   ) THEN
+       CREATE USER timeline_writer;
+   END IF;
+END
+$$;
+
+-- Grant minimal required privileges
+GRANT USAGE ON SCHEMA timeline TO timeline_writer;
+GRANT INSERT, DELETE ON timeline.locations TO timeline_writer;
