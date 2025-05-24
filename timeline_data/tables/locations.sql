ad590f459c38d9269b40974bd802efee3b8e4ac5
manoj-bhaskaran <manoj.bhaskaran@gmail.com>
2025-05-24 07:33:18 +0530

    Enhance locations table with additional partial index for non-null elevation values

--- timeline_data/tables/locations.sql
+++ timeline_data/tables/locations.sql
@@ -18,3 +18,8 @@ CREATE INDEX idx_locations_timestamp ON timeline.locations (timestamp);
 
 -- Create a spatial index on the location column for efficient spatial queries
 CREATE INDEX idx_locations_location ON timeline.locations USING GIST (location);
+
+-- Create a partial index for locations with elevation not null
+CREATE INDEX idx_locations_elevation_not_null
+ON timeline.locations (elevation)
+WHERE elevation IS NOT NULL;
